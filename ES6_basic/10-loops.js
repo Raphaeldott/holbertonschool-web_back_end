3ad7d3ce0b2416abf43ca9641ea33baa8fd2906d
raphaeldott <raphael.dott@hotmail.com>
2025-03-24 15:48:33 +0100

    fastcommit

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,6 +1,6 @@
 export default function appendToEachArrayValue(array, appendString) {
-    for (const [index, value] of array.entries()) {  // Get index and value using array.entries()
-      array[index] = appendString + value;           // Modify array element
+    for (const [index, value] of array.entries()) {
+      array[index] = appendString + value;
     }
   
     return array;
