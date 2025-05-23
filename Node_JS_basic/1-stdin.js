52e31d00eb8fb4262e32b88af636e8ec92a96f3b
raphaeldott <raphael.dott@hotmail.com>
2025-05-23 10:27:21 +0200

    fastcommit

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -10,7 +10,10 @@ console.log('Welcome to Holberton School, what is your name?');
 readLine.question('', (name) => {
   console.log(`Your name is: ${name}`);
 
-  console.log('This important software is now closing');
+  // Only print closing message if input is NOT a TTY (i.e., piped input)
+  if (!process.stdin.isTTY) {
+    console.log('This important software is now closing');
+  }
 
   readLine.close();
 });
